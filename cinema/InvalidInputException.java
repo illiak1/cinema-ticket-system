7a02461ea7db6eaebaf1638f5f0c0ad0046be49f
illiak1 <illia.k.contact1@gmail.com>
2026-04-09 14:33:02 +0100

    Update InvalidInputException.java

--- cinema/InvalidInputException.java
+++ cinema/InvalidInputException.java
@@ -1,5 +1,8 @@
+// Package declaration
 package cinema;
-
+/**
+ * Custom Exception for Input Validation
+ */
 public class InvalidInputException extends Exception {
     public InvalidInputException(String message) {
         super(message);
